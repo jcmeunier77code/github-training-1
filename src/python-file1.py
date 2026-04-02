6dc650b424ed91924b069a0d7e3295f45a2fa3c7
Jean Christophe Meunier, PhD <jmeunier@paradigm.brussels>
2026-04-02 09:53:27 +0200

    feature: set up python environment

--- src/python-file1.py
+++ src/python-file1.py
@@ -4,13 +4,35 @@ print ('hello world')
 
 print ('hello world')
 
+print ('add hello world')
+
 print ('hello world')
 
 print ('hello world')
+
+print ('add hello world')
 
 print ('hello world')
 
 print ('hello world')   
 
+print ("don't add hello world")
+
 print ('stop now')
 
+class HelloWorld:
+    def __init__(self):
+        print ('hello world')
+
+    def add_hello_world(self):
+        print ('add hello world')
+
+    def stop_now(self):
+        print ('stop now')  
+
+if __name__ == "__main__":
+    hello_world = HelloWorld()
+    hello_world.add_hello_world()
+    hello_world.stop_now()
+
+    
